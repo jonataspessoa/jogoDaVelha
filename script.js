3a2b46d35d15e546586452f63e5dab539d5ad2ef
Jonatas Pessoa <brito_jonatas@hotmail.com>
2022-11-27 00:43:47 -0300

    Contruindo lógica do jogo

--- script.js
+++ script.js
@@ -17,7 +17,6 @@ let player = '';
 let warning = '';
 let playing = 'false';
 
-reset()
 
 // functions
 
@@ -53,6 +52,7 @@ function renderSquare() {
 		let item = document.querySelector(`div[data-item=${n}`);
 			item.innerHTML = square[n];
 	}
+	checkGame()
 }
 
 
@@ -64,4 +64,52 @@ function renderInfo() {
 function togglePlayer() {
 	player = (player === 'X') ? 'O' : 'X';
 	renderInfo();
+}
+
+
+function checkGame() {
+	if(checkWinnerFor('X')) {
+		warning = '"X" venceu o jogo!';
+		playing = false;
+	} else if (checkWinnerFor('O')) {
+		warning = 'O "O" venceu o jogo!';
+		playing = false;
+	} else if (isFull()) {
+		warning = 'Empate!'
+		playing = false;
+	}
+}
+
+
+function checkWinnerFor(player) {
+	let endGame = [
+		'a1, a2, a3',
+		'b1, b2, b3',
+		'c1, c2, c3',
+
+		'a1, b1, c1',
+		'a2, b2, c2',
+		'a3, b3, c3',
+
+		'a1, b2, c3',
+		'a3, b2, c1'
+		];
+
+		for(let k in endGame) {
+		let posEnd = endGame[k].split(',');
+		let win = posEnd.every(option => square[option] === player);
+			if(win) {
+			return true;
+		}
+	}	
+	return false;
+}
+
+function isFull() {
+	for(let i in square) {
+		if(square[i] === '') {
+			return false;
+		}
+	}
+	return true;
 }
