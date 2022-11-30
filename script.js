e2c739742851573a55058167aba9eb434ce101cb
Jonatas Pessoa <brito_jonatas@hotmail.com>
2022-11-30 20:46:21 -0300

    JavaScript desenvolvido

--- script.js
+++ script.js
@@ -1,86 +1,83 @@
-//datas
-let square = {
-	a1: '', a2: '', a3: '',
-	b1: '', b2: '', b3: '',
-	c1: '', c2: '', c3: ''
+let board = {
+	a1:'', a2:'',a3:'',
+	b1:'', b2:'', b3:'',
+	c1:'', c2:'', c3:''
 };
 
-
-//events
-
-document.querySelector('.reset').addEventListener('click', reset);
-document.querySelectorAll('.item').forEach((item)=>{
-	item.addEventListener('click', itemClick);
-});
 
 let player = '';
 let warning = '';
 let playing = 'false';
 
+reset()
 
-reset();
-// functions
+document.querySelector('.reset').addEventListener('click', reset);
+document.querySelectorAll('.item').forEach((item) =>{
+	item.addEventListener('click', itemSelector);
+})
 
-function itemClick(event) {
+function itemSelector(event) {
 	let item = event.target.getAttribute('data-item');
-	if(playing && square[item] === '') {
-		square[item] = player;
-		renderSquare()
-		togglePlayer()
+	if(playing && board[item] === '') {
+		board[item] = player;
 	}
+	renderBoard();
+	changePlayer();
 }
 
+
 function reset() {
-	let random = Math.floor(Math.random() * 2)
+	warning = '';
+
+	let random = Math.floor(Math.random() * 2);
 	player = (random === 0) ? 'X' : 'O';
 
-	for (let i in square) {
-		square[i] = '';
+	for(let i in board) {
+		board[i] = '';
 	}
 
 	playing = true;
 
-	renderSquare();
 	renderInfo();
-}
-
-
-function renderSquare() {
-	for ( let n in square) {
-		let item = document.querySelector(`div[data-item=${n}`);
-			item.innerHTML = square[n];
-	}
-	checkGame()
+	renderBoard();
 }
 
 
 function renderInfo() {
 	document.querySelector('.vez').innerHTML = player;
 	document.querySelector('.resultado').innerHTML = warning;
-};
+}
 
-function togglePlayer() {
+
+function renderBoard() {
+	for(let i in board) {
+		let item = document.querySelector(`div[data-item=${i}`);
+		item.innerHTML = board[i];
+	}
+	checkEnd()
+}
+
+function changePlayer() {
 	player = (player === 'X') ? 'O' : 'X';
 	renderInfo();
 }
 
-
-function checkGame() {
-	if(checkWinnerFor('X')) {
+function checkEnd() {
+	if(checkEndGame('X')) {
 		warning = '"X" venceu!';
 		playing = false;
-	} else if (checkWinnerFor('O')) {
+	} else if (checkEndGame('O')) {
 		warning = '"O" venceu!';
 		playing = false;
 	} else if (isFull()) {
-		warning = 'Empate!'
+		warning = 'Empate!';
 		playing = false;
 	}
 }
 
 
-function checkWinnerFor(player) {
-	let endGame = [
+function checkEndGame(player) {
+	let end = [
 		'a1,a2,a3',
 		'b1,b2,b3',
 		'c1,c2,c3',
@@ -90,24 +87,25 @@ function checkWinnerFor(player) {
 		'a3,b3,c3',
 
 		'a1,b2,c3',
-		'a3,b2,c1'
+		'a3,b2,c1'	
 		];
 
-		for(let k in endGame) {
-		let posEnd = endGame[k].split(',');
-		let win = posEnd.every(option => square[option] === player);
+	for(let k in end) {
+		let endGame = end[k].split(',');
+		let win = endGame.every(options => board[options] === player);
 			if(win) {
-			return true;
+				return true;
+			}
 		}
-	}	
-	return false;
+		return false;
 }
 
 function isFull() {
-	for(let i in square) {
-		if(square[i] === '') {
+	for (let i in board) {
+		if(board[i] === '') {
 			return false;
 		}
 	}
 	return true;
 }
+
